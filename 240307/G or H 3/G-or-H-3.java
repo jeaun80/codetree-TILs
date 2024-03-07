5e85c95c419cff70eb44a9b6123f81f36f11159f
kimjingwon <82080128+jeaun80@users.noreply.github.com>
2024-03-07 15:42:10 +0900

    Title: G or H 3, Time: 146ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240307/G or H 3/G-or-H-3.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[]nkStrArr = br.readLine().split(" ");
+
+        int n = Integer.parseInt(nkStrArr[0]);
+        int k = Integer.parseInt(nkStrArr[1]);
+
+        String map[] = new String[10001];
+        
+        int min = 0;
+        for(int i=0;i<n;i++){
+
+            String[]inputArr = br.readLine().split(" ");
+            int place  = Integer.parseInt(inputArr[0]);
+            min = Math.max(place,min);
+            map[place] = inputArr[1];
+        }
+
+        int answer =0;
+
+        for(int i=1;i<min-k+1;i++){
+            int midanswer =0;
+            for(int j=i;j<=k+1;j++){
+                if(map[j]==null){
+                    continue;
+                }
+                if(map[j].equals("G")){
+                    midanswer+= 1;
+                }
+                else if(map[j].equals("H")){
+                    midanswer+=2;
+                }
+            }
+
+            answer = Math.max(midanswer,answer);
+        }
+
+        System.out.println(answer);
+    }
+}
