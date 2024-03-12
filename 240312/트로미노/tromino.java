78b2fa755e29d0706d6b33837f0016e66ed4ea1b
kimjingwon <82080128+jeaun80@users.noreply.github.com>
2024-03-12 22:15:43 +0900

    Title: 트로미노, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240312/트로미노/tromino.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int dx[] = {0,1,0,-1};
+    static int dy[] = {1,0,-1,0};
+
+    static int gx[] = {0,0,1,2};
+    static int gy[] = {1,2,0,0};
+
+
+    public static void main(String[] args) throws IOException{
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[]inputStrSizeArr = br.readLine().split(" ");
+        int n = Integer.parseInt(inputStrSizeArr[0]);
+        int m = Integer.parseInt(inputStrSizeArr[1]);
+
+        int map[][] = new int[n][m];
+
+
+        for(int i=0;i<n;i++){
+            String[]inputStrArr = br.readLine().split(" ");
+            for(int j=0;j<m;j++){
+                map[i][j] = Integer.parseInt(inputStrArr[j]);
+            }
+        }
+        int answer = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+
+                for(int r=0;r<4;r++){
+                    boolean flag = true;
+                    int midsum=map[i][j];
+                    for(int k = 0;k<2;k++){
+                        int index = (r+k)%4;
+                        int nx = i+dx[index];
+                        int ny = j+dy[index];
+                        if(nx>=0 && ny>=0 && nx<n && ny<m){
+                            midsum+=map[nx][ny];
+                        }
+                        else{
+                            flag =false;
+                            break;
+                        }
+                    }
+                    if(flag){
+                        answer = Math.max(midsum,answer);
+                    }
+                }
+
+                for(int r=0;r<2;r++){
+                    int midsum=map[i][j];
+                    boolean flag = true;
+
+                    int index =0;
+                    if(r==1){
+                        index = 1;
+                    }
+                    for(int k = 0;k<2;k++){
+                        index = r+k;
+                        int nx = i+dx[index];
+                        int ny = j+dy[index];
+                        if(nx>=0 && ny>=0 && nx<n && ny<m){
+                            midsum+=map[nx][ny];
+                        }
+                        else{
+                            flag =false;
+                            break;
+                        }
+                    }
+                    if(flag){
+                        answer = Math.max(midsum,answer);
+                    }
+                }
+                
+            }
+        }
+
+        System.out.println(answer);
+        // 여기에 코드를 작성해주세요.
+    }
+}
