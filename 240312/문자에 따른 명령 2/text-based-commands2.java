dbb5e4b16e074e7f67be0777bd0b0c1441de6a73
kimjingwon <82080128+jeaun80@users.noreply.github.com>
2024-03-12 18:11:57 +0900

    Title: 문자에 따른 명령 2, Time: 291ms, Memory: 15MB, Status: Passed - CodeTree

--- 240312/문자에 따른 명령 2/text-based-commands2.java
+++ 240312/문자에 따른 명령 2/text-based-commands2.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
 
-    static int dx[] = {-1,0,1,0};
+    static int dx[] = {1,0,-1,0};
     static int dy[] = {0,1,0,-1};
     public static void main(String[] args) throws IOException{
 
