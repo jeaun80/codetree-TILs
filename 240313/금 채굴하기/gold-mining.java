c92b9eb2b87ac867c335f84270e45166763007f6
kimjingwon <82080128+jeaun80@users.noreply.github.com>
2024-03-13 16:44:33 +0900

    Title: 금 채굴하기, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240313/금 채굴하기/gold-mining.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+        public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] grid = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int maxProfit = 0;
+        for (int K = 0; K < n; K++) {
+            int profit = calculateProfit(grid, m, K);
+            maxProfit = Math.max(maxProfit, profit);
+        }
+
+        System.out.println(maxProfit);
+    }
+
+    public static int calculateProfit(int[][] grid, int m, int K) {
+        int n = grid.length;
+        int profit = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int diamondSum = 0;
+                for (int a = -K; a <= K; a++) {
+                    for (int b = -K; b <= K; b++) {
+                        if (isValid(i + a, j + b, n) && Math.abs(a) + Math.abs(b) <= K) {
+                            diamondSum += grid[i + a][j + b];
+                        }
+                    }
+                }
+                int cost = (int)Math.pow(K, 2) + (int)Math.pow(K + 1, 2);
+                if (diamondSum * m >= cost) {
+                    profit = Math.max(profit, diamondSum);
+                }
+            }
+        }
+
+        return profit;
+    }
+
+    public static boolean isValid(int i, int j, int n) {
+        return i >= 0 && i < n && j >= 0 && j < n;
+    }
+}
