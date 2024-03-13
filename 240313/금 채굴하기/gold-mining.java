52e1cf529fba2227dab794e7130c9adc262788c9
kimjingwon <82080128+jeaun80@users.noreply.github.com>
2024-03-13 16:52:37 +0900

    Title: 금 채굴하기, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240313/금 채굴하기/gold-mining.java
+++ 240313/금 채굴하기/gold-mining.java
@@ -19,41 +19,35 @@ public class Main {
         }
 
         int maxProfit = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int K = 0; K < n; K++) {
-                    int profit = calculateProfit(grid, m, i, j, K);
-                    maxProfit = Math.max(maxProfit, profit);
-                }
-            }
+        for (int K = 0; K < n; K++) {
+            int profit = calculateProfit(grid, m, K);
+            maxProfit = Math.max(maxProfit, profit);
         }
 
         System.out.println(maxProfit);
     }
 
-    public static int calculateProfit(int[][] grid, int m, int centerX, int centerY, int K) {
+    public static int calculateProfit(int[][] grid, int m, int K) {
         int n = grid.length;
         int profit = 0;
 
-        for (int i = centerX - K; i <= centerX + K; i++) {
-            for (int j = centerY - K; j <= centerY + K; j++) {
-                if (isValid(i, j, n)) {
-                    int diamondSum = 0;
-                    boolean isValidDiamond = true;
-                    for (int a = centerX - i; a <= centerX + i; a++) {
-                        for (int b = centerY - j; b <= centerY + j; b++) {
-                            if (isValid(a, b, n)) {
-                                diamondSum += grid[a][b];
-                                if (grid[a][b] == 0) {
-                                    isValidDiamond = false;
-                                }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int diamondSum = 0;
+                boolean isValidDiamond = true;
+                for (int a = -K; a <= K; a++) {
+                    for (int b = -K; b <= K; b++) {
+                        if (isValid(i + a, j + b, n) && Math.abs(a) + Math.abs(b) <= K) {
+                            diamondSum += grid[i + a][j + b];
+                            if (grid[i + a][j + b] == 0) {
+                                isValidDiamond = false;
                             }
                         }
                     }
-                    int cost = (int) Math.pow(K, 2) + (int) Math.pow(K + 1, 2);
-                    if (isValidDiamond && diamondSum * m >= cost) {
-                        profit = Math.max(profit, diamondSum);
-                    }
+                }
+                int cost = (int)Math.pow(K, 2) + (int)Math.pow(K + 1, 2);
+                if (isValidDiamond && diamondSum * m >= cost) {
+                    profit = Math.max(profit, diamondSum);
                 }
             }
         }
