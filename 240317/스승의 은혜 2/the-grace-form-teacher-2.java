3fed4422b22ed02669a732c58f87bf56706eb05f
kimjingwon <82080128+jeaun80@users.noreply.github.com>
2024-03-17 05:02:25 +0900

    Title: 스승의 은혜 2, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240317/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        String[]inputStrArr = br.readLine().split(" ");
+
+        int n = Integer.parseInt(inputStrArr[0]);
+        int b = Integer.parseInt(inputStrArr[1]);
+
+        int giftList[] = new int[n];
+        for(int i=0;i<n;i++){
+            giftList[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(giftList);
+        int answer =0;
+        for(int i=0;i<n;i++){
+            int p = b;
+            for(int j=0;j<n;j++){
+                int gift = giftList[j];
+                if(i==j){
+                    gift/=2;
+                }
+                if(p<gift){
+                    answer = Math.max(answer,j);
+                }
+                else{
+                    p-=gift;
+                }
+            }
+        }
+        System.out.println(answer);
+
+
+        // 여기에 코드를 작성해주세요.
+    }
+}
