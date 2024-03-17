a6a890ff9115c90daaff24c5da370e8d92815639
kimjingwon <82080128+jeaun80@users.noreply.github.com>
2024-03-18 03:40:44 +0900

    Title: 숫자들의 합 중 최대, Time: 243ms, Memory: 19MB, Status: Passed - CodeTree

--- 240318/숫자들의 합 중 최대/maximum-of-sum-of-numbers.java
+++ 240318/숫자들의 합 중 최대/maximum-of-sum-of-numbers.java
@@ -14,19 +14,16 @@ public class Main {
         int a = Integer.parseInt(inputStrArr[0]);
         int b = Integer.parseInt(inputStrArr[1]);
 
-        String AstrArr[] = String.valueOf(a).split("");
-        String BstrArr[] = String.valueOf(b).split("");
+        int answer = 0;
 
-        int asum =0;
-        int bsum =0;
-        for(String i : AstrArr){
-            asum+=Integer.parseInt(i);
+        for(int i = a;i<=b;i++){
+            int sum = 0;
+            String AstrArr[] = String.valueOf(i).split("");
+            for(String s : AstrArr){
+                sum+=Integer.parseInt(s);
+            }
+            answer = Math.max(sum,answer);
         }
-
-        for(String i : BstrArr){
-            bsum+=Integer.parseInt(i);
-        }
-        int answer = Math.max(asum,bsum);
         System.out.println(answer);
         // 여기에 코드를 작성해주세요.
     }
