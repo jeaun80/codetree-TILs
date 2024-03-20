d4a7669ec0aeb5a36a751a46f78b211eff2d423b
kimjingwon <82080128+jeaun80@users.noreply.github.com>
2024-03-21 02:20:35 +0900

    Title: 팀으로 하는 틱택토 2, Time: 103ms, Memory: 9MB, Status: Passed - CodeTree

--- 240321/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
+++ 240321/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
@@ -2,6 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static class point{
+        int x;
+        int y;
+        public point(int x,int y){
+            this.x = Math.max(x,y);
+            this.y = Math.min(x,y);
+        }
+    }
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
 
@@ -20,14 +28,31 @@ public class Main {
             }
         }
         int answer =0;
-
+        Set<point>pointSet = new HashSet<>();
         Set<Integer> set = new HashSet<>();
         for(int i=0;i<3;i++){
             set.add(map[i][0]);
             set.add(map[i][1]);
             set.add(map[i][2]);
+            int a[] = new int[2];
             if(set.size()==2){
-                answer++;
+                int count =0;
+                for(int x:set){
+                    a[count++] = x;
+                }
+                int max = Math.max(a[0],a[1]);
+                int min = Math.min(a[0],a[1]);
+                boolean flag = false;
+                for(point p : pointSet){
+                    if(p.x==max && p.y==min){
+                        flag = true;
+                        break;
+                    }
+                }
+                if(!flag){
+                    pointSet.add(new point(max,min));
+                    answer++;
+                }
             }
             set.clear();
         }
@@ -35,8 +60,26 @@ public class Main {
             set.add(map[0][i]);
             set.add(map[1][i]);
             set.add(map[2][i]);
+            int a[] = new int[2];
+
             if(set.size()==2){
-                answer++;
+                int count =0;
+                for(int x:set){
+                    a[count++] = x;
+                }
+                int max = Math.max(a[0],a[1]);
+                int min = Math.min(a[0],a[1]);
+                boolean flag = false;
+                for(point p : pointSet){
+                    if(p.x==max && p.y==min){
+                        flag = true;
+                        break;
+                    }
+                }
+                if(!flag){
+                    pointSet.add(new point(max,min));
+                    answer++;
+                }
             }
             set.clear();
         }
@@ -44,16 +87,50 @@ public class Main {
         set.add(map[0][0]);
         set.add(map[1][1]);
         set.add(map[2][2]);
+        int a[] = new int[2];
+
         if(set.size()==2){
-            answer++;
-        }
+                int count =0;
+                for(int x:set){
+                    a[count++] = x;
+                }
+                int max = Math.max(a[0],a[1]);
+                int min = Math.min(a[0],a[1]);
+                boolean flag = false;
+                for(point p : pointSet){
+                    if(p.x==max && p.y==min){
+                        flag = true;
+                        break;
+                    }
+                }
+                if(!flag){
+                    pointSet.add(new point(max,min));
+                    answer++;
+                }        }
         set.clear();
         set.add(map[2][0]);
         set.add(map[1][1]);
         set.add(map[0][2]);
+        a = new int[2];
+
         if(set.size()==2){
-            answer++;
-        }
+                int count =0;
+                for(int x:set){
+                    a[count++] = x;
+                }
+                int max = Math.max(a[0],a[1]);
+                int min = Math.min(a[0],a[1]);
+                boolean flag = false;
+                for(point p : pointSet){
+                    if(p.x==max && p.y==min){
+                        flag = true;
+                        break;
+                    }
+                }
+                if(!flag){
+                    pointSet.add(new point(max,min));
+                    answer++;
+                }        }
 
         System.out.println(answer);
     }
