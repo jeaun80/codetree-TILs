f145fd9a5a96d613c7846ad671526eff904f0cdc
kimjingwon <82080128+jeaun80@users.noreply.github.com>
2024-03-21 02:06:19 +0900

    Title: 팀으로 하는 틱택토 2, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240321/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int map[][] = new int[3][3];
+        for(int i =0;i<3;i++){
+
+
+            String[]inputStrArr = br.readLine().split("");
+
+            for(int j=0;j<3;j++){
+                map[i][j] = Integer.parseInt(inputStrArr[j]); 
+            }
+        }
+        int answer =0;
+
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<3;i++){
+            set.add(map[i][0]);
+            set.add(map[i][1]);
+            set.add(map[i][2]);
+            if(set.size()==2){
+                answer++;
+            }
+            set.clear();
+        }
+        for(int i=0;i<3;i++){
+            set.add(map[0][i]);
+            set.add(map[0][i]);
+            set.add(map[0][i]);
+            if(set.size()==2){
+                answer++;
+            }
+            set.clear();
+        }
+
+        set.add(map[0][0]);
+        set.add(map[1][1]);
+        set.add(map[2][2]);
+        if(set.size()==2){
+            answer++;
+        }
+        set.clear();
+        set.add(map[2][0]);
+        set.add(map[1][1]);
+        set.add(map[0][2]);
+        if(set.size()==2){
+            answer++;
+        }
+
+        System.out.println(answer);
+    }
+}
