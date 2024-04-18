3e810dde03a32bded2dc0989fd887d39756d9c84
kimjingwon <82080128+jeaun80@users.noreply.github.com>
2024-04-19 01:58:57 +0900

    Title: 자연수 M/2개의 쌍, Time: 1389ms, Memory: 54MB, Status: Passed - CodeTree

--- 240419/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
+++ 240419/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
@@ -1,57 +1,77 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class Main {
+class Pair implements Comparable<Pair> {
+    int y, x;
+    Pair(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
 
-    public static int MAX_N = 100001;
-    public static void main(String[] args) throws IOException{
+    @Override
+    public int compareTo(Pair p) {
+        return this.y - p.y;        // y 기준 오름차순으로 정렬합니다.
+    }
+}
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {    
+    // 변수 선언
+    public static int n;
+    public static ArrayList<Pair> nums = new ArrayList<>();
+    
+    public static int ans;
 
-        int n = Integer.parseInt(br.readLine());
-
-        List<Integer>list = new ArrayList<>();
-
-        int[][]map = new int[100001][1];
-        int size =0;
-        for(int i=0;i<n;i++){
-
-            String[] inputStrArr = br.readLine().split(" ");
-
-            int su = Integer.parseInt(inputStrArr[0]);
-            int num = Integer.parseInt(inputStrArr[1]);
-
-            map[num][0] += su;
-            size+=su;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            nums.add(new Pair(y, x));
         }
 
-        int firCursur = 1;
-        int secCursur = MAX_N-1;
-        int min =0;
+        // y 기준으로 오름차순 정렬을 진행합니다.
+        Collections.sort(nums);
 
-        for(int i=0;i<size/2;i++){
-            int fir = 0;
-            int sec = 0;
-            while(map[firCursur][0]==0){
-                firCursur++;
-            }
-            if(map[firCursur][0]>0){
-                // fir = map[firCursur][0];
-                map[firCursur][0]--;
-            }
+        // 최선의 전략을 오름차순 정렬 뒤
+        // 가장 작은 수와 큰 수를 하나씩 매칭해주는 것입니다.
+        int li = 0, ri = n - 1;
+        while(li <= ri) {
+            int ly = nums.get(li).y;
+            int lx = nums.get(li).x;
+            int ry = nums.get(ri).y;
+            int rx = nums.get(ri).x;
 
-            while(map[secCursur][0]==0){
-                secCursur--;
+            // 답을 갱신합니다.
+            ans = Math.max(ans, ly + ry);
+
+            // 왼쪽 개수가 더 적다면
+            // 왼쪽을 전부 매칭시켜줍니다.
+            if(lx < rx) {
+                // 오른쪽은 lx만큼 개수를 줄여줍니다.
+                nums.set(ri, new Pair(ry, rx - lx));
+
+                // 왼쪽 위치를 한칸 뒤로 옮겨줍니다.
+                li++;
             }
-            if(map[secCursur][0]>0){
-                // sec = map[secCursur][0];
-                map[secCursur][0]--;
+            // 오른쪽 개수가 더 적다면
+            // 오른쪽을 전부 매칭시켜줍니다.
+            else if(lx > rx) {
+                // 왼쪽은 rx만큼 개수를 줄여줍니다.
+                nums.set(li, new Pair(ly, lx - rx));
+
+                // 오른쪽 위치를 한칸 앞으로 옮겨줍니다.
+                ri--;
             }
-            int compareMin = firCursur+secCursur;
-            // System.out.println(firCursur+" "+secCursur);
-            min = Math.max(compareMin,min);
+            // 개수가 동일하다면
+            // li, ri 위치 모두 옮겨줍니다.
+            else {
+                li++;
+                ri--;
+            }
         }
-        System.out.println(min);
-        // 여기에 코드를 작성해주세요.
+        System.out.print(ans);
     }
 }
