71a08e84fac6bbfa5aaef230d40be5ccf392f8df
kimjingwon <82080128+jeaun80@users.noreply.github.com>
2024-04-18 15:11:19 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240418/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer>bList = new ArrayList<>();
+        List<Integer>aList = new LinkedList<>();
+
+        for(int i=0;i<n;i++){
+            bList.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(bList);
+
+        for(int i=1;i<=2*n;i++){
+            if(!bList.contains(i)){
+                aList.add(i);
+            }
+        }
+        Collections.sort(aList);
+        int cnt = 0;
+        for(int bIndex=0;bIndex<n;bIndex++){
+            int bValue = bList.get(bIndex);
+            for(int aIndex=0;aIndex<aList.size();aIndex++){
+                if(bValue<aList.get(aIndex)){
+                    aList.remove(aIndex);
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+
+        // 여기에 코드를 작성해주세요.
+    }
+}
