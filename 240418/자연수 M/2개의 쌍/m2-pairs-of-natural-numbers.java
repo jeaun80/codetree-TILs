144020d9c4dcca24f54193fb6cfc72312f386193
kimjingwon <82080128+jeaun80@users.noreply.github.com>
2024-04-18 19:31:25 +0900

    Title: 자연수 M/2개의 쌍, Time: 2127ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240418/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer>list = new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+
+            String[] inputStrArr = br.readLine().split(" ");
+
+            int su = Integer.parseInt(inputStrArr[0]);
+            int num = Integer.parseInt(inputStrArr[1]);
+            for(int j=0;j<su;j++){
+                list.add(num);
+            }
+        }
+
+        Collections.sort(list);
+        int min = Integer.MAX_VALUE;
+        int listSize = list.size();
+
+        for(int i=0;i<list.size()/2;i++){
+            int compareMin = list.get(i)+list.get(listSize-i-1);
+            min = Math.min(compareMin,min);
+        }
+        System.out.println(min);
+        // 여기에 코드를 작성해주세요.
+    }
+}
